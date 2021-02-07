de2504409ef32e4d82370fcbe28c72bef57766e0
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2021-02-07 22:06:00 +0000

    CV 2020/2021

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -5,61 +5,65 @@ var addressPoints = [
     -16.244002941617833
   ],
   [
-    "Las Palmas de Gran Canaria",
+    "Las Palmas de Gran Canaria (Invited Lecturer, several times)",
     28.071499524297575, -15.453738659056587
   ],
   [
-    "Barcelona",
+    "Barcelona ICS '95: Proceedings of the 9th international conference on Supercomputing",
     41.39028218292509, 2.1134188420407196
   ],
   [
-    "San Luis",
+    "San Luis Invited Professor (4 times)",
     -33.29201019972548, 
     -66.33964939756142
   ],
   [
-    "Valenciennes",
+    "Valenciennes (Visitor, 3 times)",
     50.324780891116994, 
     3.5145769897114083
   ],
   [
-    "East Stroudsburg",
+    "East Stroudsburg (Visitor)",
     40.99762867909209, -75.17267035530288
   ],
   [
-    "Edinburgh",
+    "Edinburgh (TRACS Program x 2)",
     55.921823594664865, -3.1738012881694986
   ],
   [ 
-    "Southamptom",
+    "Southamptom (EuroPar)",
     50.9353120185247, -1.3957808595116128
   ],
   [
-    "Venezia",
+    "Venezia (PVM/MPI conferences, 2 times)",
     45.434483053974915, 12.338089540296808
   ],
   [
-    "Mantua",
+    "Mantua (PDP)",
     45.160497168923804, 10.788942542152851
   ],
   [ 
-    "Budapest",
+    "Budapest (Euro PVM/MPI)",
     47.48150009987077, 19.055429753864633
   ],
   [
-    "Balaton",
+    "Balaton (Euro PVM/MPI)",
     46.872962002718936, 17.912852239776896
   ],
   [
-    "Klagenfurt",
+    "Ρόδος Eight Euromicro Workshop on Parallel and Distributed Processing, PDP 2000, 19-12 January 2000",
+    36.458369696631074, 28.2263956716819
+  ],
+  [
+    "Klagenfurt (EuroPar)",
     46.635646228763385, 14.306916472320337
   ],
   [
-    "Praghe",
+    "Praghe (PDP)",
     50.092089373363635, 14.419957523166321
   ],
   [
-    "Napoli",
+    "Napoli (Invited Lecturer)",
     41.06688124711899, 14.33052386901452
   ],
   [
@@ -81,6 +85,9 @@ var addressPoints = [
   [
     "Versailles",
     48.799796587565645, 2.1416073134333127
+  ],
+  [ "Dresden PARCO 2003",
+    51.02971050602808, 13.728941831467198
   ],
   /*
   [
